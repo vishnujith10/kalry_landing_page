acb970a073bb3d0446b1bf56977c30fbf5dd4d1f
vishnujith10 <vishnujith601@gmail.com>
2025-09-10 20:09:39 +0530

    fixing

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,6 @@
-// postcss.config.js
-export default {
-    plugins: {
-      "@tailwindcss/postcss": {},
-    }
-  }
-  
+module.exports = {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+};
