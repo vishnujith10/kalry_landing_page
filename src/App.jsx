3a96ea14a9c58747d154325d0b7538702ccd4f65
vishnujith10 <vishnujith601@gmail.com>
2025-09-12 07:46:25 +0530

    icon change

--- src/App.jsx
+++ src/App.jsx
@@ -15,10 +15,13 @@ const Header = ({ activeSection, setActiveSection }) => {
     <header className="absolute top-0 left-0 right-0 z-20">
       <nav className="container mx-auto px-6 py-5 flex justify-between items-center">
         <div className="flex items-center gap-3">
-          <svg className="h-8 w-8 text-[#6D28D9]" fill="none" viewBox="0 0 48 48" xmlns="http://www.w3.org/2000/svg">
-            <path d="M44 11.2727C44 14.0109 39.8386 16.3957 33.69 17.6364C39.8386 18.877 44 21.2618 44 24C44 26.7382 39.8386 29.123 33.69 30.3636C39.8386 31.6043 44 33.9891 44 36.7273C44 40.7439 35.0457 44 24 44C12.9543 44 4 40.7439 4 36.7273C4 33.9891 8.16144 31.6043 14.31 30.3636C8.16144 29.123 4 26.7382 4 24C4 21.2618 8.16144 18.877 14.31 17.6364C8.16144 16.3957 4 14.0109 4 11.2727C4 7.25611 12.9543 4 24 4C35.0457 4 44 7.25611 44 11.2727Z" fill="currentColor" />
-          </svg>
+            <img 
+            src="/Assets/kalry_logo.png" 
+            alt="Kalry Logo" 
+            className="h-12 w-12"
+          />
           <h2 className="font-lexend text-2xl font-bold">Kalry</h2>
+          
         </div>
         
         <div className="hidden md:flex items-center gap-8 text-base">
